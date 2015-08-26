98b0f86b8a52797c5bbed578ffa3c141a824b4c2
F483 <fabian.barkhau@gmail.com>
2015-08-26 21:45:09 +0200

    use py2exe for windows build

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 import sys
-
-
 import os
 from setuptools import setup, find_packages
+
+
+# Only load py2exe settings when its used, so we can install it first.
+options = {}
+if os.name == 'nt' and 'py2exe' in sys.argv:
+    import py2exe  # NOQA
+    options = {'py2exe': {
+        "optimize": 2,
+        "bundle_files": 2,  # This tells py2exe to bundle everything
+    }}
 
 
 exec(open('dataserv_client/version.py').read())  # load __version__
@@ -26,10 +35,11 @@ setup(
     author='Shawn Wilkinson',
     author_email='shawn+dataserv-client@storj.io',
     license="MIT",
-    version=__version__,
+    version=__version__,  # NOQA
     scripts=[SCRIPT],
+    console=[SCRIPT],
     test_suite="tests",
-    dependency_links = [],
+    dependency_links=[],
     install_requires=open("requirements.txt").readlines(),
     tests_require=open("test_requirements.txt").readlines(),
     download_url=DOWNLOAD_URL,
@@ -50,5 +60,6 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ]
+    ],
+    options=options
 )
