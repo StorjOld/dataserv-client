d2540def07b451d1887cb828b40a67d8d109644f
F483 <fabian.barkhau@gmail.com>
2015-09-06 15:37:05 +0200

    fixed get filesystem type function

--- dataserv_client/util.py
+++ dataserv_client/util.py
@@ -1,13 +1,21 @@
 import psutil
+import os
 
 
-# source http://stackoverflow.com/a/25286268
-def get_fs_type(path):
-    root_type = ""
+def get_fs_type(path):  # FIXME find a way to unit test this
+    """Returns: path filesystem type or None."""
+    partitions = {}
     for partition in psutil.disk_partitions():
-        if partition.mountpoint == '/':
-            root_type = partition.fstype
-            continue
-        if path.startswith(partition.mountpoint):
-            return partition.fstype
-    return root_type
+        partitions[partition.mountpoint] = (partition.fstype, partition.device)
+    if path in partitions:
+        return partitions[path][0]
+    splitpath = path.split(os.sep)
+    for i in range(len(splitpath), 0, -1):
+        path = os.sep.join(splitpath[:i]) + os.sep
+        if path in partitions:
+            return partitions[path][0]
+        path = os.sep.join(splitpath[:i])
+        if path in partitions:
+            return partitions[path][0]
+    return None
+
