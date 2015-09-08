5ab447feba28e605ab7eb4fb2554ec985a8c2af9
F483 <fabian.barkhau@gmail.com>
2015-09-08 20:35:31 +0200

    v2.1.2

--- dataserv_client/control/config.py
+++ dataserv_client/control/config.py
@@ -86,6 +86,7 @@ _MIGRATIONS = {
     "2.0.2": lambda btctxstore, cfg: _set_version(btctxstore, cfg, '2.0.3'),
     "2.0.3": lambda btctxstore, cfg: _set_version(btctxstore, cfg, '2.1.0'),
     "2.1.0": lambda btctxstore, cfg: _set_version(btctxstore, cfg, '2.1.1'),
+    "2.1.1": lambda btctxstore, cfg: _set_version(btctxstore, cfg, '2.1.2'),
     # TODO add smarter version ranges so no new line needed for every version
 }
 
