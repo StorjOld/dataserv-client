07eaf19b93d33e6a0a7f0cb9a78039c78b8527a8
F483 <fabian.barkhau@gmail.com>
2015-09-22 10:34:36 +0200

    remove print statement

--- dataserv_client/common.py
+++ dataserv_client/common.py
@@ -31,7 +31,6 @@ if os.environ.get(_retry_delay_label):
     DEFAULT_CONNECTION_RETRY_DELAY = int(os.environ.get(_retry_delay_label))
 else:
     DEFAULT_CONNECTION_RETRY_DELAY = 30
-print("DEFAULT_CONNECTION_RETRY_DELAY", DEFAULT_CONNECTION_RETRY_DELAY)
 
 
 # logging
