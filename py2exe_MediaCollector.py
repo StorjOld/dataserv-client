d4e62e4535323e47318ba0d21e6820c363184cb2
littleskunk <jens.heimbuerge@googlemail.com>
2016-03-15 05:45:22 +0800

    windows binary workarounds

---
+++ py2exe_MediaCollector.py
@@ -0,0 +1,51 @@
+import os
+import glob
+import jsonschema
+import lib2to3
+from py2exe.build_exe import py2exe as build_exe
+
+class MediaCollector(build_exe):
+   """
+      This class Adds 
+      jsonschema files draft3.json and draft4.json
+      lib2to3 files Grammar.txt and PatternGrammar.txt
+      to the list of compiled files so it will be included in the zipfile.
+   """
+
+   def copy_extensions(self, extensions):
+      build_exe.copy_extensions(self, extensions)
+
+      # jsonschema files draft3.json and draft4.json
+
+      # Define the data path where the files reside.
+      data_path = os.path.join(jsonschema.__path__[0],'schemas','*.json')
+
+      # Create the subdir where the json files are collected.
+      media = os.path.join('jsonschema', 'schemas')
+      full = os.path.join(self.collect_dir, media)
+      self.mkpath(full)
+
+      # Copy the json files to the collection dir. Also add the copied file
+      # to the list of compiled files so it will be included in the zipfile.
+      for f in glob.glob(data_path):
+         name = os.path.basename(f)
+         self.copy_file(f, os.path.join(full, name))
+         self.compiled_files.append(os.path.join(media, name))
+
+      # lib2to3 files Grammar.txt and PatternGrammar.txt
+
+      # Define the data path where the files reside.
+      data_path = os.path.join(lib2to3.__path__[0],'*.txt')
+
+      # Create the subdir where the json files are collected.
+      media = os.path.join('lib2to3')
+      full = os.path.join(self.collect_dir, media)
+      self.mkpath(full)
+
+      # Copy the json files to the collection dir. Also add the copied file
+      # to the list of compiled files so it will be included in the zipfile.
+      for f in glob.glob(data_path):
+         name = os.path.basename(f)
+         self.copy_file(f, os.path.join(full, name))
+         self.compiled_files.append(os.path.join(media, name))
+
