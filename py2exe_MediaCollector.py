07eef56089bc8df006aed0e61fb7479c7e0f5306
littleskunk <jens.heimbuerge@googlemail.com>
2016-03-15 01:09:23 +0100

    windows binary workarounds

--- py2exe_MediaCollector.py
+++ py2exe_MediaCollector.py
@@ -15,23 +15,6 @@ class MediaCollector(build_exe):
    def copy_extensions(self, extensions):
       build_exe.copy_extensions(self, extensions)
 
-      # jsonschema files draft3.json and draft4.json
-
-      # Define the data path where the files reside.
-      data_path = os.path.join(jsonschema.__path__[0],'schemas','*.json')
-
-      # Create the subdir where the json files are collected.
-      media = os.path.join('jsonschema', 'schemas')
-      full = os.path.join(self.collect_dir, media)
-      self.mkpath(full)
-
-      # Copy the json files to the collection dir. Also add the copied file
-      # to the list of compiled files so it will be included in the zipfile.
-      for f in glob.glob(data_path):
-         name = os.path.basename(f)
-         self.copy_file(f, os.path.join(full, name))
-         self.compiled_files.append(os.path.join(media, name))
-
       # lib2to3 files Grammar.txt and PatternGrammar.txt
 
       # Define the data path where the files reside.
